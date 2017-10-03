d138280870eecf2747fffe73c2b998d802c47722
taxusorg <taxus@taxus.org>
2017-10-03 14:14:32 +0800

    config

---
+++ config/xunsearch.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'server_host' => env('XUNSEARCH_SERVER_HOST', '127.0.0.1'),
+    'server_index_port' => env('XUNSEARCH_SERVER_INDEX_PORT', '8383'),
+    'server_search_port' => env('XUNSEARCH_SERVER_SEARCH_PORT', '8384'),
+];
